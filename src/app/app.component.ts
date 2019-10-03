68a0c88daf72b37f24d2fd05991d8b2cdcc31793
Saurabh <p17saurabh@gmail.com>
2019-10-02 23:49:22 -0400

    connecting with service get users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'adminPortal';
+
+  readonly GET_USER_URL = 'https://jogrtcz86g.execute-api.us-east-2.amazonaws.com/dev/getUsers';
+
+  users : any;
+
+  constructor(private http: HttpClient){}
+    
+    getUsers(){
+      this.users = this.http.get(this.GET_USER_URL)
+      console.log(this.users);
+    
+  }
 }
